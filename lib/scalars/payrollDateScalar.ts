d64e3ffa3b450f662cb2440e36542825e941b916
Rizart Dokollari <r.dokollari@gmail.com>
2022-06-03 20:48:24 +0100

    Force date format to UTC

--- lib/scalars/payrollDateScalar.ts
+++ lib/scalars/payrollDateScalar.ts
@@ -5,9 +5,9 @@ const error = "Expected date format: 01/31/2020";
 
 export const payrollDateScalar = new GraphQLScalarType({
   name: "PayrollDate",
-  description: "Date custom scalar type. Example: 31-01-2020",
+  description: "Date only, custom scalar type. Example: 2020-01-31",
   serialize(value) {
-    return value;
+    return (value as Date).getTime();
   },
   parseValue(value) {
     if (typeof value !== "string") throw new UserInputError(error);
@@ -16,11 +16,13 @@ export const payrollDateScalar = new GraphQLScalarType({
 
     if (isNaN(date.getDay())) throw new UserInputError(error);
 
-    return value;
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(value)) throw new UserInputError(error);
+
+    return new Date(value);
   },
   parseLiteral(value) {
-    if (value.kind === Kind.STRING) return value as unknown;
+    if (value.kind === Kind.STRING) return new Date(value as unknown as string);
 
-    throw new UserInputError(error);
+    return null;
   },
 });
