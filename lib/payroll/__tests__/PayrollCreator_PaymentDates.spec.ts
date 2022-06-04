82e572adf28b4e1187d10b310d72e5c6c877ad27
Rizart Dokollari <r.dokollari@gmail.com>
2022-06-04 10:33:56 +0100

    It includes the supplied date on the returned payment date

--- lib/payroll/__tests__/PayrollCreator_PaymentDates.spec.ts
+++ lib/payroll/__tests__/PayrollCreator_PaymentDates.spec.ts
@@ -1,104 +1,18 @@
 import { PayrollCreator } from "../PayrollCreator";
 
 describe("Payment dates", () => {
-  // it.each([
-  //   "2022-01-14",
-  //   "2022-02-01",
-  //   "2022-03-01",
-  //   "2022-04-01",
-  //   "2022-05-01",
-  //   "2022-06-01",
-  //   "2022-07-01",
-  //   "2022-08-01",
-  //   "2022-09-01",
-  //   "2022-10-01",
-  //   "2022-11-01",
-  //   "2022-12-01",
-  // ])("uses subsequent %s next month when calculating payroll", (input) => {
-  //   const result = oneUpperCaseRule(input);
-  //   payrollCreator.handle(input);
-  //   expect(payrollCreator.baseSalaryDate).toHaveBeenNthCalledWith(1, );
-  // });
-
-  // it.skip("uses subsequent next 12 months dates", () => {
-  //   const { payrollCreator, date } = makeFactory();
-  //   payrollCreator.handle(date);
-  //   expect(payrollCreator.baseSalaryDate).toHaveBeenNthCalledWith();
-  // });
   it("returns payment dates for the following 12 months", () => {
-    const {
-      payrollCreator,
-      date,
-      mockedBaseSalaryDate,
-      mockedBonusSalaryDate,
-    } = makeFactory();
-
-    expect(payrollCreator.handle(date)).toEqual([
-      {
-        month: "2022-2",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-3",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-4",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-5",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-6",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-7",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-8",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-9",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-10",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-11",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2022-12",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-      {
-        month: "2023-1",
-        baseSalaryPaymentDate: mockedBaseSalaryDate,
-        bonusPaymentDate: mockedBonusSalaryDate,
-      },
-    ]);
+    const { payrollCreator, date, expectedPayrolls } =
+      makeFactoryWithPaymentDatesForTheFollowing12Months();
+    expect(payrollCreator.handle(date)).toEqual(expectedPayrolls);
   });
 
-  it.skip("includes the supplied date on the returned payment dates", () => {
-    // TODO
+  it("includes the supplied date on the returned payment dates", () => {
+    {
+      const { payrollCreator, date } = makeFactory();
+      const expected = `${date.getFullYear()}-${date.getMonth() + 1}`;
+      expect(payrollCreator.handle(date)[0].month).toEqual(expected);
+    }
   });
 });
 
@@ -121,4 +35,21 @@ function makeFactory() {
     mockedBonusSalaryDate,
     mockedBaseSalaryDate,
   };
+}
+
+function makeFactoryWithPaymentDatesForTheFollowing12Months() {
+  const factory = makeFactory();
+
+  const expectedPayrolls = [];
+  const date = new Date(factory.date.getTime());
+  for (let index = 1; index <= 12; index++) {
+    expectedPayrolls.push({
+      month: `${date.getFullYear()}-${date.getMonth() + 1}`,
+      baseSalaryPaymentDate: factory.mockedBaseSalaryDate,
+      bonusPaymentDate: factory.mockedBonusSalaryDate,
+    });
+    date.setMonth(date.getMonth() + 1);
+  }
+
+  return { ...factory, expectedPayrolls };
 }
