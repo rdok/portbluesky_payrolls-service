0d1deae723a3cad4e3fad4f24714eae684b8513f
Rizart Dokollari <r.dokollari@gmail.com>
2022-06-04 09:32:07 +0100

    It pays base salary on the previous Friday, having last day of month a Sunday

--- lib/payroll/PayrollCreator.ts
+++ lib/payroll/PayrollCreator.ts
@@ -3,10 +3,18 @@ export class PayrollCreator {
     return date;
   }
 
-  payrollForDate(date: Date) {
-    const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    return {
-      baseSalaryPaymentDate: lastDayOfMonth,
-    };
+  baseSalaryDate(date: Date) {
+    const lastDayOfMonth = date;
+    lastDayOfMonth.setDate(1);
+    lastDayOfMonth.setMonth(lastDayOfMonth.getMonth() + 1);
+    lastDayOfMonth.setDate(lastDayOfMonth.getDate() - 1);
+
+    const isSaturday = lastDayOfMonth.getDay() === 6;
+    if (isSaturday) lastDayOfMonth.setDate(lastDayOfMonth.getDate() - 1);
+
+    const isSunday = lastDayOfMonth.getDay() === 0;
+    if (isSunday) lastDayOfMonth.setDate(lastDayOfMonth.getDate() - 2);
+
+    return lastDayOfMonth;
   }
 }
--- lib/payroll/__tests__/PayrollCreator.spec.ts
+++ lib/payroll/__tests__/PayrollCreator.spec.ts
@@ -4,13 +4,24 @@ import { PayrollCreator } from "../PayrollCreator";
 describe("Base salaries", () => {
   it("pays base salaries on the last day the month", () => {
     const { payrollCreator, lastDayOfMonth, date } = makeFactory();
-    expect(payrollCreator.payrollForDate(date)).toEqual({
-      baseSalaryPaymentDate: lastDayOfMonth,
-    });
+    expect(payrollCreator.baseSalaryDate(date)).toEqual(lastDayOfMonth);
   });
 
-  it.skip("pays base salaries on the Friday when last day of month is a weekend", () => {
-    // TODO
+  describe("pays base salaries on the Friday", function () {
+    test("when last day of month is a Saturday", () => {
+      const { payrollCreator, lastFridayOfMonth, saturdayLastDayOfMonth } =
+        makeFactoryWithSaturdayLastDayOfMonth();
+      expect(payrollCreator.baseSalaryDate(saturdayLastDayOfMonth)).toEqual(
+        lastFridayOfMonth
+      );
+    });
+    test("when last day of month is a Sunday", () => {
+      const { payrollCreator, lastFridayOfMonth, sundayLastDayOfMonth } =
+        makeFactoryWithSundayLastDayOfMonth();
+      expect(payrollCreator.baseSalaryDate(sundayLastDayOfMonth)).toEqual(
+        lastFridayOfMonth
+      );
+    });
   });
 });
 
@@ -39,4 +50,17 @@ function makeFactory() {
   const lastDayOfMonth = new Date("2022-01-31");
   const date = new Date("2022-01-28");
   return { payrollCreator, date, lastDayOfMonth };
+}
+function makeFactoryWithSaturdayLastDayOfMonth() {
+  const payrollCreator = new PayrollCreator();
+  const lastFridayOfMonth = new Date("2022-04-29");
+  const saturdayLastDayOfMonth = new Date("2022-04-30");
+  return { payrollCreator, lastFridayOfMonth, saturdayLastDayOfMonth };
+}
+
+function makeFactoryWithSundayLastDayOfMonth() {
+  const payrollCreator = new PayrollCreator();
+  const lastFridayOfMonth = new Date("2022-07-29");
+  const sundayLastDayOfMonth = new Date("2022-07-31");
+  return { payrollCreator, lastFridayOfMonth, sundayLastDayOfMonth };
 }
